7a16ae3e09fe8b4caea18f1d3f6967609b48368b
patadriano <patriciamae1602@gmail.com>
2025-03-26 15:52:02 +0800

    react + api

---
+++ testreactapinet/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testreactapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        // GET: api/hello
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Hello, Worjhhjld!" });
+        }
+    }
+}
